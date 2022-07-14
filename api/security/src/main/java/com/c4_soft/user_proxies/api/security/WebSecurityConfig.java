b3cfccb69dfaa333d4d845eb35e0807cc4e1410b
ch4mpy <ch4mp@c4-soft.com>
2022-07-14 01:36:42 -1000

    spring-addons 4.5.1

--- api/security/src/main/java/com/c4_soft/user_proxies/api/security/WebSecurityConfig.java
+++ api/security/src/main/java/com/c4_soft/user_proxies/api/security/WebSecurityConfig.java
@@ -5,21 +5,21 @@ import org.springframework.security.access.expression.method.MethodSecurityExpre
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 import com.c4_soft.springaddons.security.oauth2.SynchronizedJwt2AuthenticationConverter;
-import com.c4_soft.springaddons.security.oauth2.SynchronizedJwt2ClaimSetConverter;
 import com.c4_soft.springaddons.security.oauth2.config.Jwt2AuthoritiesConverter;
+import com.c4_soft.springaddons.security.oauth2.config.Jwt2ClaimSetConverter;
 import com.c4_soft.springaddons.security.oauth2.spring.C4MethodSecurityExpressionHandler;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 	
 	@Bean
-	SynchronizedJwt2ClaimSetConverter<ProxiesClaimSet> claimsConverter() {
+	Jwt2ClaimSetConverter<ProxiesClaimSet> claimsConverter() {
 		return jwt -> new ProxiesClaimSet(jwt.getClaims());
 	}
 
     @Bean
     SynchronizedJwt2AuthenticationConverter<ProxiesAuthentication> authenticationConverter(
-    		SynchronizedJwt2ClaimSetConverter<ProxiesClaimSet> claimsConverter,
+    		Jwt2ClaimSetConverter<ProxiesClaimSet> claimsConverter,
             Jwt2AuthoritiesConverter authoritiesConverter) {
         return jwt -> new ProxiesAuthentication(claimsConverter.convert(jwt), authoritiesConverter.convert(jwt), jwt.getTokenValue());
     }
