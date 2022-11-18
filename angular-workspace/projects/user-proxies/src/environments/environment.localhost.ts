1cb324a4fe103a1c48d9f46fb1c5d9fa817759d3
ch4mpy <ch4mp@c4-soft.com>
2022-11-17 14:03:40 -1000

    Fix CORS error when going through gateway

--- angular-workspace/projects/user-proxies/src/environments/environment.localhost.ts
+++ angular-workspace/projects/user-proxies/src/environments/environment.localhost.ts
@@ -1,7 +1,7 @@
 import { LogLevel, PassedInitialConfig } from 'angular-auth-oidc-client'
 
-const usersBasePath = 'https://localhost:8080'
-const greetBasePath = 'https://localhost:8080'
+const usersBasePath = 'https://localhost:9443' // https://localhost:8080 can be used instead if gateway is up
+const greetBasePath = 'https://localhost:9445' // https://localhost:8080 can be used instead if gateway is up
 
 const secureRoutes = [
   `${usersBasePath}/users`,
@@ -10,7 +10,7 @@ const secureRoutes = [
 
 export const authConfig: PassedInitialConfig = {
   config: {
-    authority: 'https://localhost:8443/realms/master',
+    authority: 'https://localhost:8443/realms/master', // https://localhost:8080/realms/master can be used instead if gateway is up
     secureRoutes,
     redirectUrl: window.location.origin,
     postLogoutRedirectUri: window.location.origin,
--- api/gateway/src/main/java/gatewaycom/c4_soft/user_proxies/api/GatewayApplication.java
+++ api/gateway/src/main/java/gatewaycom/c4_soft/user_proxies/api/GatewayApplication.java
@@ -1,9 +1,13 @@
 package gatewaycom.c4_soft.user_proxies.api;
 
+import java.util.function.Function;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.UriSpec;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -22,14 +26,20 @@ public class GatewayApplication {
 	 * @return
 	 */
 	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes().route(p -> p.path("/users/**").uri("https://localhost:9443"))
-				.route(p -> p.path("/greet/**").uri("https://localhost:9445"))
-				.route(p -> p.path("/realms/**")
-						.filters(f -> f.removeResponseHeader("Access-Control-Allow-Origin")
-								.removeResponseHeader("Access-Control-Allow-Methods")
-								.removeResponseHeader("Access-Control-Allow-ALLOWED_HEADERS"))
-						.uri("https://localhost:8443"))
+	public RouteLocator myRoutes(RouteLocatorBuilder builder, Function<GatewayFilterSpec, UriSpec> corsFilters) {
+		return builder.routes().route(p -> p.path("/users/**").filters(corsFilters).uri("https://localhost:9443"))
+				.route(p -> p.path("/greet/**").filters(corsFilters).uri("https://localhost:9445"))
+				.route(p -> p.path("/realms/**").filters(corsFilters).uri("https://localhost:8443"))
 				.build();
+	}
+
+	@Bean
+	Function<GatewayFilterSpec, UriSpec> corsFilters() {
+		// use something more restrictive in production
+		return f -> f
+				.setResponseHeader("Access-Control-Allow-Origin", "*")
+				.setResponseHeader("Access-Control-Allow-Methods", "*")
+				.setResponseHeader("Access-Control-Allow-headers", "*")
+				.setResponseHeader("Access-Control-Expose-Headers", "*");
 	}
 }
